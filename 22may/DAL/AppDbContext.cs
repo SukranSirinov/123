2620a12cb572f1d28d5cfd6c7cbc4bccc1ba4bca
SukranSirinov <tu7nwumeg@code.edu.az>
2022-06-24 01:53:24 +0400

    Add project files.

---
+++ 22may/DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using _22may.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _22may.DAL.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Portofolio> Portofolios { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ 22may/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace _22may.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+}
